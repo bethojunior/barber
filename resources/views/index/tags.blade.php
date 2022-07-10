1f44fd6c96f56b857f4cfb9b746e5c87b38b13a4
Betho <hgomescostajr@gmail.com>
2022-07-10 11:17:36 -0300

    nav

--- resources/views/index/tags.blade.php
+++ resources/views/index/tags.blade.php
@@ -1,11 +1,11 @@
 <meta charset="utf-8" />
 <meta name="theme-color" content="#D3CBC8">
-<link rel="icon" href="{{ asset('assets/images/logo/logo.png') }}">
+<link rel="icon" href="{{ asset('img/poles.png') }}">
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 <meta name="description" content="Madgic Soluções " />
 <meta property="og:title" content="Madgic Soluções " />
 <meta property="og:type" content="article" />
 <meta property="og:url" content="https://madgic.com.br" />
-<meta property="og:image" content="https://drajessycamartins.madgic.com.br/assets/images/logo/me.jpg" />
+<meta property="og:image" content="{{ asset('img/poles.png') }}" />
 <meta property="og:description" content="Madgic Soluções" />
 <meta property="og:site_name" content="Madgic Soluções" />
